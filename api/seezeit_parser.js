1e4b0f112f225fdace77bb906ba7ac73b135a2b7
Sam <earthstamper@gmail.com>
2020-06-22 14:22:20 +0200

    set up debugging Co-authored-by: FunnyPocketBook <ying-kai.dang@outlook.com>

--- api/seezeit_parser.js
+++ api/seezeit_parser.js
@@ -36,39 +36,49 @@ function convertXmltoJson(xml) {
             let tags = [];
             item.icons[0].split(",").forEach((icon) => {
                 switch (icon) {
-                    case "23": tags.push("B");
-                    break;
-                    case "24": tags.push("Vegan");
-                    break;
-                    case "45": tags.push("Sch");
-                    break; 
-                    case "46": tags.push("R");
-                    break; 
-                    case "47": tags.push("R");
-                    break; 
-                    case "48": tags.push("L");
-                    break; 
-                    case "49": tags.push("G");
-                    break; 
-                    case "50": tags.push("F");
-                    break;
-                    case "51": tags.push("Veg");
-                    break; 
-                    case "52": tags.push("W");
-                    break; 
-                }                    
+                    case "23":
+                        tags.push("B");
+                        break;
+                    case "24":
+                        tags.push("Vegan");
+                        break;
+                    case "45":
+                        tags.push("Sch");
+                        break;
+                    case "46":
+                        tags.push("R");
+                        break;
+                    case "47":
+                        tags.push("R");
+                        break;
+                    case "48":
+                        tags.push("L");
+                        break;
+                    case "49":
+                        tags.push("G");
+                        break;
+                    case "50":
+                        tags.push("F");
+                        break;
+                    case "51":
+                        tags.push("Veg");
+                        break;
+                    case "52":
+                        tags.push("W");
+                        break;
+                }
             });
 
             if (item.title && item.title[0].includes("Fleischbällchen")) {
                 item.title[0] = item.title[0].replace("Fleischbällchen", "leischbällchen");
-            } 
+            }
             let dish = {
                 Name: item.title ? item.title[0] : "",
-                Category: item.category ? item.category[0]: "",
+                Category: item.category ? item.category[0] : "",
                 Pricing: item.preis1 ? item.preis1[0] + " €" : "",
-                PricingSchool: item.preis2 ? item.preis2[0] + " €": "",
+                PricingSchool: item.preis2 ? item.preis2[0] + " €" : "",
                 PricingEmp: item.preis3 ? item.preis3[0] + " €" : "",
-                PricingGuest: item.preis4 ? item.preis4[0] + " €": "",
+                PricingGuest: item.preis4 ? item.preis4[0] + " €" : "",
                 Tags: tags
             }
 
@@ -146,47 +156,47 @@ function filterMenu(menus, excludeSup, includeTags) {
 }
 
 function handleApiV2Get(req, res) {
-  if ((req.query.includeTags && req.query.includeTags.length != 0) || req.query.excludeSup.length != 0) {
-      console.log("API v2: GET received from " + req.ip + " for " + req.query.mensa + " with query " + "exclude_sups=(" + req.query.excludeSup + ") include_tags=(" + req.query.includeTags + ")");
-      console.log("API v2: User Agent: " + req.get('User-Agent'));
-  } else {
-      console.log("API v2: GET received from " + req.ip + " for " + req.query.mensa)
-      console.log("API v2: User Agent: " + req.get('User-Agent'));
-  }
-  http.get(req.query.langURL, (httpRes) => {
-      const { statusCode } = httpRes;
-      const contentType = httpRes.headers['content-type'];
+    if ((req.query.includeTags && req.query.includeTags.length != 0) || req.query.excludeSup.length != 0) {
+        console.log("API v2: GET received from " + req.ip + " for " + req.query.mensa + " with query " + "exclude_sups=(" + req.query.excludeSup + ") include_tags=(" + req.query.includeTags + ")");
+        console.log("API v2: User Agent: " + req.get('User-Agent'));
+    } else {
+        console.log("API v2: GET received from " + req.ip + " for " + req.query.mensa)
+        console.log("API v2: User Agent: " + req.get('User-Agent'));
+    }
+    http.get(req.query.langURL, (httpRes) => {
+        const { statusCode } = httpRes;
+        const contentType = httpRes.headers['content-type'];
 
-      let error;
-      if (statusCode !== 200) {
-          error = new Error('Request Failed.\n' +
-              `Status Code: ${statusCode}`);
-      } else if (!/^[a-zA-Z]+\/xml/.test(contentType)) {
-          error = new Error('Invalid content-type.\n' +
-              `Expected [a-zA-Z]/xml but received ${contentType}`);
-      }
-      if (error) {
-          console.error(error.message);
-          // Consume response data to free up memory
-          httpRes.resume();
-          return;
-      }
+        let error;
+        if (statusCode !== 200) {
+            error = new Error('Request Failed.\n' +
+                `Status Code: ${statusCode}`);
+        } else if (!/^[a-zA-Z]+\/xml/.test(contentType)) {
+            error = new Error('Invalid content-type.\n' +
+                `Expected [a-zA-Z]/xml but received ${contentType}`);
+        }
+        if (error) {
+            console.error(error.message);
+            // Consume r1ponse data to free up memory
+            httpRes.resume();
+            return;
+        }
 
-      httpRes.setEncoding('utf8');
-      let rawData = '';
-      httpRes.on('data', (chunk) => { rawData += chunk; });
-      httpRes.on('end', () => {
-          parseString(rawData, function (err, result) {
-              if (err) {
-                  res.send("it's all fucked up m8").status(503);
-                  return;
-              }
-              res.json(filterMenu(convertXmltoJson(result), req.query.excludeSup, req.query.includeTags)).status(200);
-          });
-      });
-  }).on('error', (e) => {
-      console.error(`Got error: ${e.message}`);
-  });
+        httpRes.setEncoding('utf8');
+        let rawData = '';
+        httpRes.on('data', (chunk) => { rawData += chunk; });
+        httpRes.on('end', () => {
+            parseString(rawData, function(err, result) {
+                if (err) {
+                    res.send("it's all fucked up m8").status(503);
+                    return;
+                }
+                res.json(filterMenu(convertXmltoJson(result), req.query.excludeSup, req.query.includeTags)).status(200);
+            });
+        });
+    }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+    });
 }
 
 function parseQuery(req, res, next) {
@@ -249,6 +259,6 @@ function convertQueryToXmlFormat(req, res, next) {
 app.get("/", cors(), parseQuery, convertQueryToXmlFormat, handleApiV2Get);
 
 export default {
-  handler: app,
-  path: '/api'
+    handler: app,
+    path: '/api'
 }
